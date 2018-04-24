54df46fd78aa4f8ffb8efc34171b866ff2b2d245
Nick <38404646+otsonick@users.noreply.github.com>
2018-04-25 02:49:54 +1000

    Add files via upload

---
+++ NG_html_files/1.js
@@ -0,0 +1,48 @@
+window.local_imageGallerySimple_v11_1_data = [
+{
+ border: "4",
+ cols: "4",
+ effect: "0",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "NG_html_files/21.jpg"
+},
+{
+ image: "NG_html_files/22.jpg"
+},
+{
+ image: "NG_html_files/23.jpg"
+},
+{
+ image: "NG_html_files/24.jpg"
+},
+{
+ image: "NG_html_files/25.jpg"
+},
+{
+ image: "NG_html_files/26.jpg"
+},
+{
+ image: "NG_html_files/27.jpg"
+},
+{
+ image: "NG_html_files/28.jpg"
+},
+{
+ image: "NG_html_files/29.jpg"
+},
+{
+ image: "NG_html_files/30.jpg"
+},
+{
+ image: "NG_html_files/31.jpg"
+},
+{
+ image: "NG_html_files/32.jpg"
+}
+];
---
+++ NG_html_files/2.js
@@ -0,0 +1,27 @@
+window.local_imageGallerySimple_v11_4_data = [
+{
+ border: "4",
+ cols: "4",
+ effect: "0",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "NG_html_files/34.jpg"
+},
+{
+ image: "NG_html_files/35.jpg"
+},
+{
+ image: "NG_html_files/36.jpg"
+},
+{
+ image: "NG_html_files/37.jpg"
+},
+{
+ image: "NG_html_files/38.jpg"
+}
+];
